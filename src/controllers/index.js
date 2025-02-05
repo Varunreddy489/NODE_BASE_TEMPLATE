38a85693435b7a888efed57237ea09fc4da4ad39
Varunreddy <varunsannapureddy@gmail.com>
2025-02-06 00:35:59 +0530

    new file:   .gitignore 	new file:   Readme.md 	new file:   package-lock.json 	new file:   package.json 	new file:   src/config/index.js 	new file:   src/config/logger.config.js 	new file:   src/config/server-config.js 	new file:   src/controllers/index.js 	new file:   src/index.js 	new file:   src/middleware/index.js 	new file:   src/repositories/index.js 	new file:   src/routes/index.js 	new file:   src/routes/v1/index.js 	new file:   src/services/index.js 	new file:   src/utils/index.js

---
+++ src/controllers/index.js
@@ -0,0 +1,5 @@
+export const info = (req, res) => {
+  return res.json({
+    success: true,
+  });
+};
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+import { apiRoutes } from "./routes/index.js";
+import { PORT,logger } from "./config/index.js";
+
+const app=express();
+
+app.use('/api',apiRoutes)
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+    console.log("Jai Shree Ram");
+    logger.info(`Server is running on port ${PORT}`)
+}) 
